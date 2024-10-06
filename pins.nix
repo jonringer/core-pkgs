7227e5f4ef558d733257bf25fc6d6ca2712bb829
Jonathan Ringer <jonringer117@gmail.com>
2024-10-05 18:16:04 -0700

    Bump stdenv, remove cloog

--- pins.nix
+++ pins.nix
@@ -1,7 +1,7 @@
 {
   stdenvRepo = builtins.fetchGit {
     url = "https://github.com/jonringer/stdenv.git";
-    rev = "6b2b03e324d50abdf15f2fa687f20a3a2798be6e";
+    rev = "8593af2030421f4668b7aefe38c9aecf63c6effa";
   };
 
   lib = import (builtins.fetchGit {
