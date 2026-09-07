7b898b7bbe916f42e6ae37c445d1c024644d2596
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 11:32:17 -0700

    abseil-cpp: switch to YYYYMMDD variant naming, add compat aliases
    Remove duplicate YYYYMM variants (v202401, v202407, v202508) that
    overlap with new YYYYMMDD variants. Keep v202103 as it has no
    YYYYMMDD equivalent. Add aliases so old names still resolve.

--- pkgs-many/abseil-cpp/default.nix
+++ pkgs-many/abseil-cpp/default.nix
@@ -2,8 +2,13 @@
 
 mkManyVariants {
   variants = ./variants.nix;
-  aliases = { };
-  defaultSelector = (p: p.v202508);
+  aliases = {
+    # Compat aliases: old YYYYMM names -> new YYYYMMDD names
+    v202401 = "v20240116";
+    v202407 = "v20240722";
+    v202508 = "v20250814";
+  };
+  defaultSelector = (p: p.v20250814);
   genericBuilder = ./generic.nix;
   inherit callPackage;
 }
--- pkgs-many/abseil-cpp/variants.nix
+++ pkgs-many/abseil-cpp/variants.nix
@@ -1,16 +1,4 @@
 {
-  v202508 = {
-    version = "20250814.2";
-    src-hash = "sha256-PCospVD/hnbT/87tRtvick+RIuwAv7DDPGnLG3ZMb3g=";
-  };
-  v202407 = {
-    version = "20240722.2";
-    src-hash = "sha256-PuS7MLwi824c4z4Cubh029DEUVYSNPD3MwCDsgzsp3Y=";
-  };
-  v202401 = {
-    version = "20240116.3";
-    src-hash = "sha256-VfC8kQtGlOew9iVKxQ7kIgqFMvHiDpSBhvyNOfneuwo=";
-  };
   v202103 = {
     version = "20210324.2";
     src-hash = "sha256-fcxPhuI2eL/fnd6nT11p8DpUNwGNaXZmd03yOiZcOT0=";
