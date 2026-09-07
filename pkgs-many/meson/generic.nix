74576de45fc3513569216d807eb657016173987f
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 09:33:18 -0700

    meson: port to mkManyVariants with v1_11 (default) and v1_12
    Meson 1.12 breaks nix builds due to a build/ directory name
    collision with nix's src/libstore/build/ subdirectory. Keep v1_11
    as the default while making v1_12 available for packages that need
    it.
    
    Uses patch semver-strategy since variants track major.minor (v1_11,
    v1_12).

--- pkgs-many/meson/generic.nix
+++ pkgs-many/meson/generic.nix
@@ -1,4 +1,10 @@
 {
+  version,
+  hash,
+  ...
+}:
+
+{
   lib,
   stdenv,
   fetchFromGitHub,
@@ -21,28 +27,31 @@
   harfbuzz,
 }:
 
+let
+  patchDir = ./patches;
+in
 python3.pkgs.buildPythonApplication rec {
   pname = "meson";
-  version = "1.12.0";
+  inherit version;
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "mesonbuild";
     repo = "meson";
     tag = version;
-    hash = "sha256-lnuySM7aCojPU9bQI7LPKgod8otFa+Spo9yIDFbOJVs=";
+    inherit hash;
   };
 
   patches = [
     # Nixpkgs cmake uses NIXPKGS_CMAKE_PREFIX_PATH for the search path
-    ./000-nixpkgs-cmake-prefix-path.patch
+    "${patchDir}/000-nixpkgs-cmake-prefix-path.patch"
 
     # In typical distributions, RPATH is only needed for internal libraries so
     # meson removes everything else. With Nix, the locations of libraries
     # are not as predictable, therefore we need to keep them in the RPATH.
     # At the moment we are keeping the paths starting with /nix/store.
     # https://github.com/NixOS/nixpkgs/issues/31222#issuecomment-365811634
-    (replaceVars ./001-fix-rpath.patch {
+    (replaceVars "${patchDir}/001-fix-rpath.patch" {
       inherit (builtins) storeDir;
     })
 
@@ -51,28 +60,28 @@ python3.pkgs.buildPythonApplication rec {
     # the risk of potentially breaking the ELF files.
     # But this can cause much bigger problem for Nix as it can produce
     # cut-in-half-by-\0 store path references.
-    # Let’s just clear the whole rpath and hope for the best.
-    ./002-clear-old-rpath.patch
+    # Let's just clear the whole rpath and hope for the best.
+    "${patchDir}/002-clear-old-rpath.patch"
 
     # Meson is currently inspecting fewer variables than autoconf does, which
     # makes it harder for us to use setup hooks, etc.
     # https://github.com/mesonbuild/meson/pull/6827
-    ./003-more-env-vars.patch
+    "${patchDir}/003-more-env-vars.patch"
 
     # Unlike libtool, vanilla Meson does not pass any information about the path
     # library will be installed to to g-ir-scanner, breaking the GIR when path
     # other than ${!outputLib}/lib is used.
     # We patch Meson to add a --fallback-library-path argument with library
     # install_dir to g-ir-scanner.
-    ./004-gir-fallback-path.patch
+    "${patchDir}/004-gir-fallback-path.patch"
 
     # Patch out default boost search paths to avoid impure builds on
     # unsandboxed non-NixOS builds, see:
     # https://github.com/NixOS/nixpkgs/issues/86131#issuecomment-711051774
-    ./005-boost-Do-not-add-system-paths-on-nix.patch
+    "${patchDir}/005-boost-Do-not-add-system-paths-on-nix.patch"
 
     # This edge case is explicitly part of meson but is wrong for nix
-    ./007-freebsd-pkgconfig-path.patch
+    "${patchDir}/007-freebsd-pkgconfig-path.patch"
   ];
 
   postPatch =
@@ -181,6 +190,7 @@ python3.pkgs.buildPythonApplication rec {
   env.hostPlatform = stdenv.targetPlatform.system;
   passthru = {
     configurePhaseHook = ./setup-hook.sh;
+    ekapkgs-update.semver-strategy = "patch";
     tests = {
       version = testers.testVersion {
         package = meson;
@@ -207,4 +217,3 @@ python3.pkgs.buildPythonApplication rec {
     identifiers.cpeParts = lib.meta.cpeFullVersionWithVendor "mesonbuild" version;
   };
 }
-# TODO: a more Nixpkgs-tailoired test suite
---
+++ pkgs-many/meson/variants.nix
@@ -0,0 +1,10 @@
+{
+  v1_11 = {
+    version = "1.11.2";
+    hash = "sha256-HGXNrw39TfNio64BH0DhSAj8zz6XLwuG0RhOfCxT2PU=";
+  };
+  v1_12 = {
+    version = "1.12.0";
+    hash = "sha256-lnuySM7aCojPU9bQI7LPKgod8otFa+Spo9yIDFbOJVs=";
+  };
+}
