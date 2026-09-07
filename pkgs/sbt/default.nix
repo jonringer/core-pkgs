e9894f7f8ba0f70dcb75111c776e5ebfb4e18b34
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 01:41:00 -0700

    sbt: 2.0.6 -> 2.0.8

--- pkgs/sbt/default.nix
+++ pkgs/sbt/default.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sbt";
-  version = "2.0.6";
+  version = "2.0.8";
 
   src = fetchurl {
     url = "https://github.com/sbt/sbt/releases/download/v${finalAttrs.version}/sbt-${finalAttrs.version}.tgz";
-    hash = "sha256-YM54pQtya1szKlJ342PWfAKPFqOhUVf3ikFsCylJvG0=";
+    hash = "sha256-QMKjF4bTaihvDZ6L/fURgeVAjE+Z6rhVVuK81eEuq0Q=";
   };
 
   postPatch = ''
