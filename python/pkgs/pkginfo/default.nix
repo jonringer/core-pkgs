1d1aa9349c9107663933d3f666becd1b122a8009
Jonathan Ringer <jonringer117@gmail.com>
2026-09-06 23:28:14 -0700

    python312Packages.pkginfo: 1.12.1.2 -> 1.13

--- python/pkgs/pkginfo/default.nix
+++ python/pkgs/pkginfo/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pkginfo";
-  version = "1.12.1.2";
+  version = "1.13";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-XNlXgkrDbxQCYJZOujxr5kQqg1m4xI9K35AhDzOgS3s=";
+    hash = "sha256-T3BHHGQ/cf6EoGIwwOM8XFsSozsv0hv/FN2u9al0rbE=";
   };
 
   build-system = [ setuptools ];
