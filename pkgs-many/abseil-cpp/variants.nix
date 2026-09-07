29c56e8b0c1007570f6965c124fd7853f7cee7f3
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 11:31:00 -0700

    abseil-cpp.v20260817: init at 20260817.0

--- pkgs-many/abseil-cpp/variants.nix
+++ pkgs-many/abseil-cpp/variants.nix
@@ -55,4 +55,8 @@
     version = "20260526.0";
     src-hash = "sha256-O9ClnGm4WSTX3g1Q2VYTMhUtGG52XBwxzgHtWW9WSG0=";
   };
+  v20260817 = {
+    version = "20260817.0";
+    src-hash = "sha256-CEtLO4il9/jk+bFDDV0rXeX1OkirA0u6nxrSiWq0NPM=";
+  };
 }
