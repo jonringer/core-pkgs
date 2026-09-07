14a5205a4e4b62bddb80e972be2726586f5eee9a
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 10:28:17 -0700

    protobuf: update v25 25.8->25.9, v29 29.5->29.6, v33 33.2->33.6

--- pkgs-many/protobuf/variants.nix
+++ pkgs-many/protobuf/variants.nix
@@ -4,16 +4,16 @@
     hash = "sha256-VZQEFHq17UsTH5CZZOcJBKiScGV2xPJ/e6gkkVliRCU=";
   };
   v25 = {
-    version = "25.8";
-    hash = "sha256-UUUblMKN1xFknFtanq5MRVQ57dzhOV9sPqlw1J37aNE=";
+    version = "25.9";
+    hash = "sha256-Psv9dJxrHnlWTLZF+FwjqB/Yw2r8DWdcuvzB9vv0Nok=";
   };
   v27 = {
     version = "27.5";
     hash = "sha256-wUXvdlz19VYpFGU9o0pap/PrwE2AkopLZJVUqfEpJVI=";
   };
   v29 = {
-    version = "29.5";
-    hash = "sha256-nraqBM87DJYU69Zx0dV9OMnI0Jhn8Axg8DvzUQDQ90Y=";
+    version = "29.6";
+    hash = "sha256-IIFqEKOlyQMuxzW3HNhCNM/x1gYmW7gaNu7IxY3VMAU=";
   };
   v30 = {
     version = "30.2";
@@ -28,7 +28,7 @@
     hash = "sha256-wfu1MyCycGpxFB++eicA0F41j886/Y52I/4+ciRUg2o=";
   };
   v33 = {
-    version = "33.2";
-    hash = "sha256-SguWBa9VlE15C+eLzcqqusVLgx9kDyPXwYImSE75HCM=";
+    version = "33.6";
+    hash = "sha256-Uoj1Q5D+ofWFc/GPOgOHgIWVwKrpJrpkHMpL+yU0/k8=";
   };
 }
