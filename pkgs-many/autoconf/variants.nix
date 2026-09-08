3c527462dd0ff08637fd5ac872bc23cc286bb2e4
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 06:33:15 -0700

    Revert bad variant bumps: autoconf269, libpng12, scala_3

--- pkgs-many/autoconf/variants.nix
+++ pkgs-many/autoconf/variants.nix
@@ -1,7 +1,7 @@
 {
   v2_69 = {
-    version = "2.73";
-    src-hash = "sha256-n9ZyschCX6wvpn+gR3uZCYcmi5D/NtXwFtrle+DWtS4=";
+    version = "2.69";
+    src-hash = "sha256-ZOvOyfisWySHElqGp3YNJZGsnh09vVlIljP53mKldoQ=";
   };
   v2_71 = {
     version = "2.71";
