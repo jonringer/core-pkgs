3d5692228d40660bf5e5dee1a270c875d52849c8
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 06:58:53 -0700

    wayland: use patch update strategy

--- pkgs-many/wayland/generic.nix
+++ pkgs-many/wayland/generic.nix
@@ -117,6 +117,8 @@ stdenv.mkDerivation (finalAttrs: {
     tests.pkg-config = testers.hasPkgConfigModules {
       package = finalAttrs.finalPackage;
     };
+  } // {
+    ekapkgs-update.semver-strategy = "patch";
   };
 
   meta = {
