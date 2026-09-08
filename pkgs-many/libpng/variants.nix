db3250a158548415b25b3ccd27d02d82f4b28573
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 19:12:49 -0700

    libpng: 1.6.50 -> 1.6.58

--- pkgs-many/libpng/variants.nix
+++ pkgs-many/libpng/variants.nix
@@ -4,8 +4,8 @@
     hash = "sha256-tGNfFbitzMitCTTupIXvWcxMriTQ8DAKmpQeUZdP/Mc=";
   };
   v1_6 = {
-    version = "1.6.50";
-    hash = "sha256-TfOWUYYgp6o2UUQ+h9Gyhi5OiMrRNai5NCPgFwYjIwc=";
-    apng-patch-hash = "sha256-aH3cDHyxKKPqWOFZtRKSUlN8J+3gwyqT8R8DEn8MAWU=";
+    version = "1.6.58";
+    hash = "sha256-KOtAP1Hw90BSSRMs7P6C6lwO+X8bMsWmWCiBSuDTR3U=";
+    apng-patch-hash = "sha256-7ufeoi7VAoaAF5cchsY8TtHmCF3guuv9zD0zIvAPPrA=";
   };
 }
