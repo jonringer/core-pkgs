a0552c33b6f290a53c06729a26816af3993a48de
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 06:56:30 -0700

    kotlin: use patch update strategy

--- pkgs-many/kotlin/generic.nix
+++ pkgs-many/kotlin/generic.nix
@@ -57,6 +57,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   passthru = {
+    ekapkgs-update.semver-strategy = "patch";
     inherit java;
     majorVersion = lib.versions.major version;
     minorVersion = lib.versions.majorMinor version;
