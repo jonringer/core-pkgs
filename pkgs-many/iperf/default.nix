49695fc47590a2fc84297f417aafb369aa135a00
Aliaksandr <grubian2@gmail.com>
2026-09-09 03:39:35 +0300

    iperf: drop

--- pkgs-many/iperf/default.nix
+++
@@ -1,9 +0,0 @@
-{ mkManyVariants, callPackage }:
-
-mkManyVariants {
-  variants = ./variants.nix;
-  aliases = { };
-  defaultSelector = (p: p.v3);
-  genericBuilder = ./generic.nix;
-  inherit callPackage;
-}
