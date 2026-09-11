5f493e5c48da3561e5d606fe6e91e48686f89174
Jonathan Ringer <jonringer117@gmail.com>
2026-09-11 12:39:03 -0700

    lua: fix variant cross-splicing with allowAliases=false

--- pkgs-many/lua/interpreter.nix
+++ pkgs-many/lua/interpreter.nix
@@ -208,10 +208,10 @@ stdenv.mkDerivation (
           luaAttr
           ;
         executable = "lua";
-        luaOnBuildForBuild = override pkgsBuildBuild.${luaAttr};
-        luaOnBuildForHost = override pkgsBuildHost.${luaAttr};
-        luaOnBuildForTarget = override pkgsBuildTarget.${luaAttr};
-        luaOnHostForHost = override pkgsHostHost.${luaAttr};
+        luaOnBuildForBuild = override (pkgsBuildBuild.${luaAttr} or self);
+        luaOnBuildForHost = override (pkgsBuildHost.${luaAttr} or self);
+        luaOnBuildForTarget = override (pkgsBuildTarget.${luaAttr} or self);
+        luaOnHostForHost = override (pkgsHostHost.${luaAttr} or self);
         luaOnTargetForTarget = lib.optionalAttrs (lib.hasAttr luaAttr pkgsTargetTarget) (
           override pkgsTargetTarget.${luaAttr}
         );
