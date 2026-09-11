1910c670fbd96944615b0161c1d29bfd0a9a8690
Jonathan Ringer <jonringer117@gmail.com>
2026-09-11 11:31:27 -0700

    gumbo: init at 0.13.2

---
+++ pkgs/gumbo/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  stdenv,
+  fetchFromCodeberg,
+  autoreconfHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gumbo";
+  version = "0.13.2";
+
+  src = fetchFromCodeberg {
+    owner = "gumbo-parser";
+    repo = "gumbo-parser";
+    rev = finalAttrs.version;
+    hash = "sha256-8mri7mLZkuIZgzE6p0yc41bNNyzGTV9V90OiA/9TkkU=";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  meta = {
+    description = "C99 HTML parsing algorithm";
+    homepage = "https://codeberg.org/gumbo-parser/gumbo-parser";
+    platforms = with lib.platforms; linux ++ darwin;
+    license = lib.licenses.asl20;
+  };
+})
