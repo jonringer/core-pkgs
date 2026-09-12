263f4f233baaf1ab7391e55a7bee062c8bfce64e
Jonathan Ringer <jonringer117@gmail.com>
2026-09-12 16:02:02 -0700

    nix-prefetch-scripts: make unavailable deps optional

--- pkgs/nix-prefetch-scripts/default.nix
+++ pkgs/nix-prefetch-scripts/default.nix
@@ -1,8 +1,8 @@
 {
   lib,
   buildEnv,
-  nix-prefetch-cvs,
-  nix-prefetch-darcs,
+  nix-prefetch-cvs ? null,
+  nix-prefetch-darcs ? null,
   nix-prefetch-git,
   nix-prefetch-hg,
   nix-prefetch-svn,
@@ -11,14 +11,15 @@
 buildEnv {
   name = "nix-prefetch-scripts";
 
-  paths = [
-    nix-prefetch-cvs
-    nix-prefetch-darcs
-    nix-prefetch-git
-    nix-prefetch-hg
-    nix-prefetch-svn
-    nix-prefetch-pijul
-  ];
+  paths =
+    lib.optional (nix-prefetch-cvs != null) nix-prefetch-cvs
+    ++ lib.optional (nix-prefetch-darcs != null) nix-prefetch-darcs
+    ++ [
+      nix-prefetch-git
+      nix-prefetch-hg
+      nix-prefetch-svn
+      nix-prefetch-pijul
+    ];
 
   meta = {
     description = "Collection of all the nix-prefetch-* scripts which may be used to obtain source hashes";
