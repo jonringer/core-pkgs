a2ff5540466ba82de090faa5ed845c39890c7855
Jonathan Ringer <jonringer117@gmail.com>
2026-09-11 21:21:39 -0700

    elixir: 1.18.1 -> 1.18.5, add v1_19 and v1_20

--- pkgs-many/elixir/variants.nix
+++ pkgs-many/elixir/variants.nix
@@ -10,12 +10,20 @@
   };
 
   v1_18 = {
-    version = "1.18.1";
+    version = "1.18.5";
     # Elixir 1.18+ requires OTP 27+
-    src-hash = "sha256-QjWmPGFcfHh9haUWfbKKWOyfWlefmz/YU/xvTYhsIJ4=";
+    src-hash = "sha256-iZbrgWl+2E3eQeitjwpSaA3lLO81MjymyVI/ZoNR2HM=";
     minimumOTPVersion = "27";
     # The default erlang (v27) is actually OTP 29, which is incompatible.
     # Pin to erlang v28 (OTP 28) which satisfies the OTP 27+ requirement.
     erlangVariant = "v28";
+  };
+  v1_19 = {
+    version = "1.19.6";
+    src-hash = "sha256-nCfADrnohmbOejxXbBshYIdaVtsQ6ApJokqFaMhk0HE=";
+  };
+  v1_20 = {
+    version = "1.20.4";
+    src-hash = "sha256-L4e+FwJYPsvu6CwK1NY1PelkY8+g+m51V+BfaNkNqGk=";
   };
 }
