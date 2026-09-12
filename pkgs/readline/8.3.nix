87c9a71b9e760bad892efe7f8f2dfc37f5f3385e
Jonathan Ringer <jonringer117@gmail.com>
2026-09-12 16:02:08 -0700

    readline: make termcap optional

--- pkgs/readline/8.3.nix
+++ pkgs/readline/8.3.nix
@@ -5,7 +5,7 @@
   fetchurl,
   updateAutotoolsGnuConfigScriptsHook,
   ncurses,
-  termcap,
+  termcap ? null,
   curses-library ? if stdenv.hostPlatform.isWindows then termcap else ncurses,
   bash,
   sqlite,
