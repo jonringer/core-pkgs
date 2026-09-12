ca12db59562c515b2d47f37be22c8696a0236dc4
Jonathan Ringer <jonringer117@gmail.com>
2026-09-11 21:21:30 -0700

    erlang: add v29 (29.0.6)

--- pkgs-many/erlang/variants.nix
+++ pkgs-many/erlang/variants.nix
@@ -11,4 +11,8 @@
     version = "28.5.0.5";
     hash = "sha256-dlAQcVdKYNsxY7XHMPgr25/X/1hEQP/jXUnj4IgvHIc=";
   };
+  v29 = {
+    version = "29.0.6";
+    hash = "sha256-k5AsIz5c86XP2lA/MF9NwEJ5hD5Ln/uHqZvjhhsHBwI=";
+  };
 }
