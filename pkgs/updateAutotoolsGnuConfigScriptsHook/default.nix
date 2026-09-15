3688957f8954e7f4ab09280d92cdef3880606fac
Jonathan Ringer <jonringer117@gmail.com>
2026-09-15 08:17:56 -0700

    updateAutotoolsGnuConfigScriptsHook: move from build-support to pkgs

--- pkgs/updateAutotoolsGnuConfigScriptsHook/default.nix
+++ pkgs/updateAutotoolsGnuConfigScriptsHook/default.nix
@@ -1,17 +1,8 @@
-{
-  lib,
-  makeSetupHook,
-  gnu-config,
-}:
+{ makeSetupHook, gnu-config }:
 
 makeSetupHook {
   name = "update-autotools-gnu-config-scripts-hook";
   substitutions = {
     gnu_config = gnu-config;
-  };
-  meta = {
-    description = "Setup hook for updating GNU config scripts";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
   };
 } ./update-autotools-gnu-config-scripts.sh
